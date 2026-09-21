978b707cd93b2b28449dc8b7547dbb5942edd7c4
Pedro Henrique Alves <118781462+pedrohenriquealvesfernandes@users.noreply.github.com>
2026-09-21 14:20:09 -0300

    Refactor DDL script for Bronze schema creation

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,39 +1,87 @@
-/* 
-
-Criação da B.dados e Schemas
-
-Nesse script, é criado uma nova b.dados chamada DataWarehouse, após verificar se ela já existe, caso ela já exista, será dropada e recriada.
-Além disso, também realizado a criação dos schemas: "bronze", "silver" e "gold".
-
+/*
+===============================================================================
+Script DDL: Criação da tabela Bronze
+===============================================================================
+    Esse script verifica se a tabela bronze existe, caso exista, ele remove a tabela é removida e recriada, caso contrário apenas cria a tabela.
+===============================================================================
 */
 
-USE master;
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
 GO
 
--- Verificar se existe alguma b.dados chamada DataWarehouse, caso sim, ela será dropada.
-
-IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE.
-	DROP DATABASE DataWarehouse;
-END;
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
 GO
 
--- Criar e usar a b.dados DataWarehouse
-
-CREATE DATABASE DataWarehouse;
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
 GO
 
-USE DataWarehouse;
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
 GO
 
--- Criação dos Schemas
-
-CREATE SCHEMA bronze;
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
 GO
 
-CREATE SCHEMA silver;
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
 GO
 
-CREATE SCHEMA gold;
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
 GO
